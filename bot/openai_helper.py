c963f6a1debe904a2fb596c6d1bf909ec072a228
fga401 <36651740+fga401@users.noreply.github.com>
2024-05-11 22:09:16 +0800

    remove token stats

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -23,9 +23,9 @@ from plugin_manager import PluginManager
 # Models can be found here: https://platform.openai.com/docs/models/overview
 # Models gpt-3.5-turbo-0613 and  gpt-3.5-turbo-16k-0613 will be deprecated on June 13, 2024
 GPT_3_MODELS = ("gpt-3.5-turbo", "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613")
-GPT_3_16K_MODELS = ("gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-0125")
+GPT_3_16K_MODELS = ("gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-0125", "deepseek-coder")
 GPT_4_MODELS = ("gpt-4", "gpt-4-0314", "gpt-4-0613")
-GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613")
+GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613", "deepseek-chat")
 GPT_4_VISION_MODELS = ("gpt-4-vision-preview",)
 GPT_4_128K_MODELS = ("gpt-4-1106-preview","gpt-4-0125-preview","gpt-4-turbo-preview")
 GPT_ALL_MODELS = GPT_3_MODELS + GPT_3_16K_MODELS + GPT_4_MODELS + GPT_4_32K_MODELS + GPT_4_VISION_MODELS + GPT_4_128K_MODELS
@@ -645,6 +645,7 @@ class OpenAIHelper:
         :param messages: the messages to send
         :return: the number of tokens required
         """
+        return len(messages)
         model = self.config['model']
         try:
             encoding = tiktoken.encoding_for_model(model)
